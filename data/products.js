9ce8e20523a5f98e78268d284f9a2ad3d0b3ed8b
httpsanuj1807 <anuj1316.be22@chitkara.edu.in>
2024-09-17 23:50:44 +0530

    add products data

---
+++ data/products.js
@@ -0,0 +1,168 @@
+export const products = [
+    {
+      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      image: "",
+      name: "Banana Blossom Vegan Meat 400gm",
+      rating: {
+        stars: 4,
+        count: 87
+      },
+      priceCents: 4102,
+      keywords: ["meat", "healthy", "wellness", "protein"]
+    },
+    {
+      id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      image: "",
+      name: "Fresh Carrot Juice 800ml 1-Serving",
+      rating: {
+        stars: 5,
+        count: 127
+      },
+      priceCents: 8203,
+      keywords: ["vegetables", "juices", "beverages", "carrot"]
+    },
+    {
+      id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+      image: "",
+      name: "Organic Leafy Carrots 2kg",
+      rating: {
+        stars: 4,
+        count: 56
+      },
+      priceCents: 2020,
+      keywords: ["carrot", "vegetables", "organic", "grocery"]
+    },
+    {
+      id: "54e0eccd-8f36-462b-b68a-8182611d9add",
+      image: "",
+      name: "Nutritious Mixed Fruits Drink 4-pack",
+      rating: {
+        stars: 5,
+        count: 2197
+      },
+      priceCents: 4900,
+      keywords: ["beverages", "juices", "healthy", "fruits"]
+    },
+    {
+      id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+      image: "",
+      name: "Juicy Watermelon",
+      rating: {
+        stars: 4.5,
+        count: 37
+      },
+      priceCents: 6508,
+      keywords: ["fruits", "juices", "healthy", "organic"]
+    },
+    {
+      id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
+      image: "",
+      name: "Fresh Breads",
+      rating: {
+        stars: 3.5,
+        count: 175
+      },
+      priceCents: 5121,
+      keywords: ["health", "bread", "eggs", "dairy"]
+    },
+    {
+      id: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
+      image: "",
+      name: "Sun Warrior Protein Blend",
+      rating: {
+        stars: 4,
+        count: 317
+      },
+      priceCents: 2805,
+      keywords: ["juices", "beverages", "fruits", "health"]
+    },
+    {
+      id: "77919bbe-0e56-475b-adde-4f24dfed3a04",
+      image: "",
+      name: "Delicious Red Pomegranates 1kg",
+      rating: {
+        stars: 4,
+        count: 144
+      },
+      priceCents: 3489,
+      keywords: ["fruits", "pomegranates", "healthy", "organic"]
+    },
+    {
+      id: "3fdfe8d6-9a15-4979-b459-585b0d0545b9",
+      image: "",
+      name: "Juicy Leafy Oranges 2kg",
+      rating: {
+        stars: 5,
+        count: 305
+      },
+      priceCents: 8710,
+      keywords: ["fruits", "juices", "natural", "organic"]
+    },
+    {
+      id: "58b4fc92-e98c-42aa-8c55-b6b79996769a",
+      image: "",
+      name: "Nutritious Broccoli",
+      rating: {
+        stars: 4,
+        count: 89
+      },
+      priceCents: 3765,
+      keywords: ["broccoli", "vegetables", "organic", "natural"]
+    },
+    {
+      id: "5968897c-4d27-4872-89f6-5bcb052746d7",
+      image: "",
+      name: "Organic Onions",
+      rating: {
+        stars: 5,
+        count: 235
+      },
+      priceCents: 3865,
+      keywords: ["vegetables", "onion", "organic", "natural"]
+    },
+    {
+      id: "aad29d11-ea98-41ee-9285-b916638cac4a",
+      image: "",
+      name: "Bolognese Tofu Sauce",
+      rating: {
+        stars: 5,
+        count: 30
+      },
+      priceCents: 9825,
+      keywords: ["tofu", "tomato", "vegetables", "healthy"]
+    },
+    {
+      id: "04701903-bc79-49c6-bc11-1af7e3651358",
+      image: "",
+      name: "Organic Tomatoes - Perfect for Salads and Sauces",
+      rating: {
+        stars: 3,
+        count: 232
+      },
+      priceCents: 3841,
+      keywords: ["tomato", "vegetables", "organic", "natural"]
+    },
+    {
+      id: "901eb2ca-386d-432e-82f0-6fb1ee7bf969",
+      image: "",
+      name: "Mixed Kiwi Broccoli and Guava Juice",
+      rating: {
+        stars: 5,
+        count: 160
+      },
+      priceCents: 6290,
+      keywords: ["beverages", "juices", "fruits", "healthy"]
+    },
+    {
+      id: "82bb68d7-ebc9-476a-989c-c78a40ee5cd9",
+      image: "",
+      name: "Fresh Crisp Apples - Nature Perfect Snack",
+      rating: {
+        stars: 3,
+        count: 846
+      },
+      priceCents: 2305,
+      keywords: ["fruits", "natural", "organic", "healthy"]
+    }
+  ]
+  
