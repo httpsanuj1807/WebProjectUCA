da56b65ca6b34630a6db6fb2af6cf69d5fa70f9f
httpsanuj1807 <anuj1316.be22@chitkara.edu.in>
2024-09-18 16:10:47 +0530

    add some imgs and delivery options

---
+++ data/deliveryOptions.js
@@ -0,0 +1,16 @@
+export const deliveryOptions=[{
+    id:'7',
+    priceCents:0,
+    deliveryDays:7
+},
+{
+    id:'3',
+    priceCents:499,
+    deliveryDays:3
+},
+{
+    id:'1',
+    priceCents:999,
+    deliveryDays:1
+}
+]
